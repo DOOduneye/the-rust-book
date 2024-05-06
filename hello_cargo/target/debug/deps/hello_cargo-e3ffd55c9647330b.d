39bcd4a2e0a1699c1002ec573af88db111a30d64
David Oduneye <davidoduneye1@gmail.com>
2024-05-06 02:09:21 -0400

    feat: added gitignore

--- hello_cargo/target/debug/deps/hello_cargo-e3ffd55c9647330b.d
+++
@@ -1,5 +0,0 @@
-/Users/davidoduneye/projects/the-rust-book/hello_cargo/target/debug/deps/libhello_cargo-e3ffd55c9647330b.rmeta: src/main.rs
-
-/Users/davidoduneye/projects/the-rust-book/hello_cargo/target/debug/deps/hello_cargo-e3ffd55c9647330b.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/deps/ppv_lite86-2a317792b98818f7.d
+++
@@ -1,8 +0,0 @@
-/Users/davidoduneye/projects/the-rust-book/hello_cargo/target/debug/deps/libppv_lite86-2a317792b98818f7.rmeta: /Users/davidoduneye/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/lib.rs /Users/davidoduneye/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/soft.rs /Users/davidoduneye/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/types.rs /Users/davidoduneye/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/generic.rs
-
-/Users/davidoduneye/projects/the-rust-book/hello_cargo/target/debug/deps/ppv_lite86-2a317792b98818f7.d: /Users/davidoduneye/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/lib.rs /Users/davidoduneye/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/soft.rs /Users/davidoduneye/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/types.rs /Users/davidoduneye/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/generic.rs
-
-/Users/davidoduneye/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/lib.rs:
-/Users/davidoduneye/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/soft.rs:
-/Users/davidoduneye/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/types.rs:
-/Users/davidoduneye/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/generic.rs:
